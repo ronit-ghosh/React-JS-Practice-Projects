e93ff1587285ba3176907e0acc73488e39097efa
ronit-ghosh <ronitghosh06@gmail.com>
2024-08-05 11:39:27 +0530

    API Key Removed

--- Contact-App/src/config/firebase.js
+++ Contact-App/src/config/firebase.js
@@ -1,19 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAB8Nyn_hBqimcoucN2bQUMqEtjw_M9EHs",
-  authDomain: "contact-app-e4c1f.firebaseapp.com",
-  projectId: "contact-app-e4c1f",
-  storageBucket: "contact-app-e4c1f.appspot.com",
-  messagingSenderId: "700145594578",
-  appId: "1:700145594578:web:c7361901347afa5accbd0b"
+  apiKey: "process.env.API_KEY;",
+  authDomain: "process.env.AUTH_DOMAIN",
+  projectId: "process.env.PROD_ID",
+  storageBucket: "process.env.STORAGE_BUCKET",
+  messagingSenderId: "process.env.MSG_SENDER_ID",
+  appId: "process.env.APP_ID"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
